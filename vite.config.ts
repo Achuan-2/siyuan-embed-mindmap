0e9634d49b78965a9f6461dd54fcd4540e5dd1eb
Tron <achuan-2@outlook.com>
2025-12-11 08:57:28 +0800

    📝

--- vite.config.ts
+++ vite.config.ts
@@ -36,8 +36,8 @@ export default defineConfig({
                 { src: "./preview.png", dest: "./" },
                 { src: "./icon.png", dest: "./" },
                 { src: "./assets/*", dest: "./assets/" },
-                { src: "./mindmap-embed/**", dest: "./mindmap-embed/" },
-                { src: "./i18n/**", dest: "./i18n/" },
+                { src: "./mindmap-embed/*", dest: "./mindmap-embed/" },
+                { src: "./i18n/*", dest: "./i18n/" },
             ],
         }),
 
